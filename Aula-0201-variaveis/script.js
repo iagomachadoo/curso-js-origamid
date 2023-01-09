c1c2f0116560cd221ce254b2d4a94eac18c11115
iagomachadoo <iago.machado000@gmail.com>
2023-01-09 20:32:59 -0300

    Aula-0201-variaveis

---
+++ Aula-0201-variaveis/script.js
@@ -0,0 +1,22 @@
+//Exercícios
+
+// Declarar uma variável com o seu nome
+var meuNome = 'Iago';
+console.log(meuNome);
+// Declarar uma variável com a sua idade
+var minhaIdade = 26;
+console.log(minhaIdade);
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var minhaComidaFavorita;
+console.log(minhaComidaFavorita);
+// Atribuir valor a sua comida favorita
+minhaComidaFavorita = 'lasanha';
+console.log(minhaComidaFavorita);
+// Declarar 5 variáveis diferentes sem valores
+var a,
+    b,
+    c,
+    d,
+    e
+console.log(a, b, c, d, e);
