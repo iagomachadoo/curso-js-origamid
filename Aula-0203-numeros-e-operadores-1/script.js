5eb97b20cfa5d9bb7110d084e54b793e741315bc
iagomachadoo <iago.machado000@gmail.com>
2023-01-09 21:40:17 -0300

    Aula-0203-numeros-e-operadores-1

---
+++ Aula-0203-numeros-e-operadores-1/script.js
@@ -0,0 +1,20 @@
+var exponencial = 2e2;
+console.log(exponencial);
+
+var exponencial2 = 2e10;
+console.log(exponencial2);
+
+var exponencial3 = 2e-3;
+console.log(exponencial3);
+
+var soma = '100' + 50;
+console.log(soma);
+
+var divisao = "100" / 50;
+console.log(divisao);
+
+var subtracao = "100" - 50;
+console.log(subtracao, isNaN(subtracao));
+
+var multiplicacao = "Comprei 100" * 50;
+console.log(multiplicacao, isNaN(multiplicacao));
