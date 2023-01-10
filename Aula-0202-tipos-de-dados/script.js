43916145e954eaa227d387e4427b228afcc18ec7
iagomachadoo <iago.machado000@gmail.com>
2023-01-09 21:10:38 -0300

    Aula-0202-tipos-de-dados

---
+++ Aula-0202-tipos-de-dados/script.js
@@ -0,0 +1,32 @@
+var nome = 'Fulano';
+console.log(nome, typeof nome);//typeof retorna o tipo de dado de uma variável
+
+var idade = 26;
+console.log(idade, typeof idade);
+
+//Exercício
+
+// Declare uma variável contendo uma string
+var string = 'Essa variável é uma string'
+console.log(string);
+
+// Declare uma variável contendo um número dentro de uma string
+var string2 = '2056'
+console.log(string2);
+
+// Declare uma variável com a sua idade
+var minhaIdade = 26
+console.log(minhaIdade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Iago';
+var sobrenome = 'Machado';
+console.log(`${nome} ${sobrenome}`);
+
+// Coloque a seguinte frase em uma variável: It's time
+var string3 = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipo = typeof nome;
+console.log(verificarTipo);
